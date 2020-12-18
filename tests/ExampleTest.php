f9e31248828769990d0878d0791443a7a932364a
Rkallenkoot <Rkallenkoot@users.noreply.github.com>
2020-12-18 16:23:25 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,22 +13,21 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
 
-
     /** @test */
     public function testYeet()
     {
         $icepay = app(Icepay::class);
 
         $testPayment = $icepay->checkout([
-            'Amount'        => 1000,
-            'Currency'      => 'EUR',
+            'Amount' => 1000,
+            'Currency' => 'EUR',
             'Paymentmethod' => 'IDEAL',
-            'Issuer'        => 'ABNAMRO',
-            'Country'       => 'NL',
-            'Language'      => 'NL',
-            'Description'   => 'This is a example description',
-            'OrderID'       =>  (string)Str::uuid(),
-            'Reference'     => '1'
+            'Issuer' => 'ABNAMRO',
+            'Country' => 'NL',
+            'Language' => 'NL',
+            'Description' => 'This is a example description',
+            'OrderID' => (string)Str::uuid(),
+            'Reference' => '1',
         ]);
 
         dump($testPayment);
